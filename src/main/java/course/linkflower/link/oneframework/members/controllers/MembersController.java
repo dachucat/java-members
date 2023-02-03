ec435c9354163f87812ad1ebebb1f77c08e942da
kee <flaymankee@gmail.com>
2023-02-03 19:35:14 +0700

    init

---
+++ src/main/java/course/linkflower/link/oneframework/members/controllers/MembersController.java
@@ -0,0 +1,11 @@
+package course.linkflower.link.oneframework.members.controllers;
+
+public class MembersController {
+
+    /*
+    测试是否登陆
+     */
+    public boolean isLogin() {
+        return false;
+    }
+}
