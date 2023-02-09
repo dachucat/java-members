311bec4040346ed0d6af6c81bae5dbc5ec25286f
javafromzero <2917462604@qq.com>
2023-02-09 22:42:18 +0800

    milk修改误输解决后

--- src/main/java/course/linkflower/link/oneframework/MembersApp.java
+++ src/main/java/course/linkflower/link/oneframework/MembersApp.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class Memb00ersApp {
+public class MembersApp {
     public static void main(String[] args) {
-        SpringApplication.run(Memb00ersApp.class, args);
+        SpringApplication.run(MembersApp.class, args);
     }
 }
