e35920fe11cb9b930e016b91f27e925f24d1295e
kee <flaymankee@gmail.com>
2023-02-11 23:24:18 +0700

    添加用户登陆

--- src/main/java/course/linkflower/link/oneframework/members/controller/MembersController.java
+++ src/main/java/course/linkflower/link/oneframework/members/controller/MembersController.java
@@ -2,13 +2,11 @@ package course.linkflower.link.oneframework.members.controller;
 
 import course.linkflower.link.oneframework.members.model.SysRoleMenu;
 import course.linkflower.link.oneframework.members.service.SysRoleMenuService;
+import course.linkflower.link.oneframework.members.vo.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,8 +35,13 @@ public class MembersController {
         return false;
     }
 
-    @GetMapping("getAllMenus")
+    @GetMapping("/getAllMenus")
     public List<SysRoleMenu> getAllMenus() {
         return sysRoleMenuService.getAllMenus();
+    }
+
+    @PostMapping("/login")
+    public UserInfo login() {
+        return new UserInfo();
     }
 }
