0b8101c3cfa00b6ffc14380559113801c5bb5612
kee <flaymankee@gmail.com>
2023-02-11 23:28:31 +0700

    错误代码提交退回

--- src/main/java/course/linkflower/link/oneframework/members/controller/MembersController.java
+++ src/main/java/course/linkflower/link/oneframework/members/controller/MembersController.java
@@ -41,19 +41,5 @@ public class MembersController {
     public List<SysRoleMenu> getAllMenus() {
         return sysRoleMenuService.getAllMenus();
     }
-    @GetMapping
-    /*
-    .
-    .
-    .
-    .
-
-    ,
-    ,
-    ,
-    ,
-    ,
-    ,
-
-     */
+    
 }
