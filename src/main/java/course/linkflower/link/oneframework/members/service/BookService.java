fb0fb18a6b04a028c1b61d99fbe92273e097a3d8
javafromzero <2917462604@qq.com>
2023-02-20 17:58:44 +0800

    Revert "增加bookputaway的api,但无法正常运行"
    This reverts commit 08a6d909c7fda3a1374568a9c7b4bd4b8dae89ee.

--- src/main/java/course/linkflower/link/oneframework/members/service/BookService.java
+++
@@ -1,7 +0,0 @@
-package course.linkflower.link.oneframework.members.service;
-
-import course.linkflower.link.oneframework.members.dto.book.PutawayBookDto;
-
-public interface BookService {
-    void putawayBook(PutawayBookDto putawayBookDto);
-}
