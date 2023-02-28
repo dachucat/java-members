e2f5e13805374120f156181a45355531aed01f44
javafromzero <2917462604@qq.com>
2023-02-28 20:02:12 +0800

    增加park数据库

--- sqls/003_58ini.sql
+++ sqls/003_58ini.sql
@@ -13,3 +13,18 @@ CREATE TABLE `topic` (
 
 --- 2023-02-27
 ALTER TABLE library.topic ADD code varchar(32) NULL;
+
+---2023-02-28
+CREATE TABLE `park` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `icon` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `introduction` text,
+  `barcode` varchar(36) DEFAULT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` tinyint unsigned NOT NULL DEFAULT '3' COMMENT '类型',
+  `ordering` bigint unsigned DEFAULT NULL,
+  `code` varchar(50) DEFAULT NULL COMMENT '与前台约定',
+  `parent_id` bigint unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `park_id_IDX` (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
